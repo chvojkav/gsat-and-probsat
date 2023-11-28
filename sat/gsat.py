b6ff7c2fc5d36e4c7d43ce2201347af4d4364d5f
Your Name <chvojvoj@fit.cvut.cz>
2023-11-28 11:22:25 +0100

    Speed refactor for gsat

--- sat/gsat.py
+++ sat/gsat.py
@@ -7,31 +7,7 @@ from pysat.formula import CNF
 from pysat.solvers import Solver
 
 
-from sat import Configuration, FormulaHelper, get_unsatisfied_clauses
-
-
-def _get_best_flips(formula: CNF,
-                    config: Configuration,
-                    helper: FormulaHelper) -> list[int]:
-    best_change = len(formula.clauses)  # worst case is that we newly usatisfy all clauses.
-    best_flips = []
-    for i in range(1, formula.nv + 1):
-        orig_unsat_clauses_of_var = len(get_unsatisfied_clauses(helper[i], config))
-        config.flip_variable(i)
-        new_unsat_clauses_of_var = len(get_unsatisfied_clauses(helper[i], config))
-        config.flip_variable(i)
-        change = new_unsat_clauses_of_var - orig_unsat_clauses_of_var
-        if change == best_change:
-            best_flips.append(i)
-        elif change < best_change:
-            best_flips = [i,]
-        
-    assert len(best_flips) != 0
-    return best_flips
-    
-
-
-
+from sat import Configuration, FormulaHelper, get_unsatisfied_clauses, clause_list
 
 
 class Gsat:
@@ -50,6 +26,7 @@ class Gsat:
         self.best_satisfied_count = 0
         self.solved = False
         self.working_config = Configuration(formula.nv)
+        self.counts_of_satisfied_literals: list[int] = []
     
     def run(self) -> tuple[int, int, list[int]]:
         for try_no in range(self.max_tries):
@@ -61,19 +38,20 @@ class Gsat:
         # -----------------------------
         # verify:
         with Solver(bootstrap_with=self.formula) as solver:
-            assert solver.solve(assumptions=self.best_config.variable_evaluation) == self.solved
+            assert solver.solve(assumptions=self.best_config.get_evaluation()) == self.solved
 
         # -----------------------------
         # print results:
         iteration_count = try_no * self.max_iters + iter_cnt + 1
 
-        return iteration_count, self.best_satisfied_count, self.best_config.variable_evaluation
+        return iteration_count, self.best_satisfied_count, self.best_config.get_evaluation()
 
     def _do_gsat_try(self) -> tuple[Configuration, int]:
         self.working_config.set_random()
+        self.calculate_counts_of_satisfied_literals()
         unsatisfied_clauses = None
         for iter_no in range(self.max_iters):
-            unsatisfied_clauses = get_unsatisfied_clauses(self.formula, self.working_config)
+            unsatisfied_clauses = self.get_unsatisfied_clauses()
             satisfied_count = self.clauses_cnt - len(unsatisfied_clauses)
 
             if satisfied_count >= self.best_satisfied_count:
@@ -90,26 +68,85 @@ class Gsat:
                 # randomly pick a variable in that clause.
                 variable = random.choice(clausule)
                 # flip the truth assignment of the chosen variable.
-                self.working_config.flip_variable(variable)
+                self.flip_variable(variable)
             else:
                 # randomly pick any variable in T
                 # whose value flip results in greatest
                 # decrease (can be 0 or negative)
                 # in the number of unsatisfied clauses.
-                best_flips = _get_best_flips(self.formula, self.working_config, self.helper)
+                best_flips = self._get_best_flips()
                 variable = random.choice(best_flips)
-                self.working_config.flip_variable(variable)
+                self.flip_variable(variable)
         
         return iter_no
+    
+    def calculate_counts_of_satisfied_literals(self):
+        self.counts_of_satisfied_literals = []
+        for clause in self.formula:
+            count_of_satisfied_literals = 0
+            for variable in clause:
+                if self.working_config.evaluate_variable(variable):
+                    count_of_satisfied_literals += 1
+            self.counts_of_satisfied_literals.append(count_of_satisfied_literals)
+    
+    def flip_variable(self, variable_name: int):
+        self.working_config.flip_variable(variable_name)
+
+        # Recalculate satisfied literals counts.
+        for clause, clause_index in self.helper[variable_name]:
+            new_sat_cnt = 0
+            for variable in clause:
+                new_sat_cnt += self.working_config.evaluate_variable(variable)
+            
+            self.counts_of_satisfied_literals[clause_index] = new_sat_cnt
+    
+    def sat_change_if_fliped(self, variable_name: int) -> int:
+        # Returns hypotetical change in number of satisfied clauses.
+        # Positive means more would be satisfied. negative means less would be satisfied.
+        change = 0
+        for clause, clause_index in self.helper[variable_name]:
+            new_sat_cnt = 0
+            for variable in clause:
+                new_sat_cnt += self.working_config.evaluate_variable(variable)
+            
+            if self.counts_of_satisfied_literals[clause_index] == 0 \
+                    and new_sat_cnt != 0:
+                change += 1
+            elif self.counts_of_satisfied_literals[clause_index] != 0 \
+                    and new_sat_cnt == 0:
+                change -= 1
+        
+        return change
+
+    def get_unsatisfied_clauses(self) -> clause_list:
+        ret = []
+        for clause, sat_cnt in zip(self.formula, self.counts_of_satisfied_literals):
+            if sat_cnt == 0:
+                ret.append(clause)
+        
+        return ret
+
+    def _get_best_flips(self) -> list[int]:
+        best_change = -self.clauses_cnt  # worst case is that we newly usatisfy all clauses.
+        best_flips = []
+        for i in range(1, self.formula.nv + 1):
+            change = self.sat_change_if_fliped(i)
+            if change == best_change:
+                best_flips.append(i)
+            elif change > best_change:
+                best_flips = [i,]
+            
+        assert len(best_flips) != 0
+        return best_flips
 
 
 def gsat(formula: CNF,
-         probability: float,
-         max_tries: int,
-         max_iters: int):
+        probability: float,
+        max_tries: int,
+        max_iters: int):
     solver = Gsat(formula, probability, max_tries, max_iters)
     return solver.run()
-    
+
 
 def main(args):
     parser = ArgumentParser()
--- sat/sat.py
+++ sat/sat.py
@@ -1,11 +1,14 @@
 import random
 from copy import deepcopy
 from typing import Iterable
+import numpy
 
 from pysat.formula import CNF
 
 clause = list[int]
 clause_list = Iterable[clause]
+# Helps to identify clauses in the original formula
+enumerated_clause_list = Iterable[tuple[clause, int]]
 
 class Configuration:
     def __init__(self, variable_cnt: int):
@@ -13,7 +16,7 @@ class Configuration:
         self.variable_evaluation: list[bool] | None = None
     
     def set_random(self):
-        self.variable_evaluation = []
+        self.variable_evaluation = [None]
         for i in range(self._variable_cnt):
             is_true = random.choice((True, False))
             evaluation = i + 1
@@ -21,44 +24,53 @@ class Configuration:
                 evaluation *= -1
 
             self.variable_evaluation.append(evaluation)
+        
+        # Now copy the evaluation reversed to support negative indexes.
+        cpy = deepcopy(self.variable_evaluation)
+        cpy.reverse()
+        self.variable_evaluation.extend(cpy)
+        self.variable_evaluation.pop() # Pop the final None
     
     def from_config(self, other: "Configuration"):
         self._variable_cnt = other._variable_cnt
         self.variable_evaluation = deepcopy(other.variable_evaluation)
 
     def flip_variable(self, variable_name: int):
-        variable_index = abs(variable_name) - 1
+        variable_index = abs(variable_name)
         self.variable_evaluation[variable_index] *= -1
+        self.variable_evaluation[-variable_index] *= -1
 
     def evaluate_variable(self, variable_name: int) -> bool:
         # variable_name is f.e. "2" or "-1" or "-15".
         # So it contains variable name plus evaluation.
         # Checks if the variable evaluation is in current configuration.
-        variable_index = abs(variable_name) - 1
-        return variable_name == self.variable_evaluation[variable_index]
+        return variable_name == self.variable_evaluation[variable_name]
+    
+    def get_evaluation(self) -> list[int]:
+        return self.variable_evaluation[1 : self._variable_cnt + 1]
 
 
 class FormulaHelper:
     def __init__(self, formula: CNF):
         # Contains list of clauses for each variable.
-        self.clauses_per_var: dict[int, clause_list] = {}
+        self.clauses_per_var: dict[int, enumerated_clause_list] = {}
         for i in range(1, formula.nv + 1):
             tmp = self._find_clauses_for_var(formula, i)
             self.clauses_per_var[i] = tmp
             self.clauses_per_var[-i] = tmp
     
     @staticmethod
-    def _find_clauses_for_var(formula: clause_list, variable_no: int) -> clause_list:
+    def _find_clauses_for_var(formula: clause_list, variable_no: int) -> enumerated_clause_list:
         clauses = []
-        for clause in formula:
+        for i, clause in enumerate(formula):
             for variable in clause:
                 if variable_no == abs(variable):
-                    clauses.append(clause)
+                    clauses.append((clause, i))
                     break
         
         return clauses
     
-    def __getitem__(self, key) -> clause_list:
+    def __getitem__(self, key) -> enumerated_clause_list:
         return self.clauses_per_var[key]
 
 
